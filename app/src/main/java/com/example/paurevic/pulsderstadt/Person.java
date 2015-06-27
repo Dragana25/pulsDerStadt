df35bc2d0d11b3b48538e080e0c23e2cb4a8815e
Dragana25 <dragana.paurevic@gmail.com>
2015-06-27 16:48:48 +0200

    testorojekt

---
+++ app/src/main/java/com/example/paurevic/pulsderstadt/Person.java
@@ -0,0 +1,25 @@
+package com.example.paurevic.pulsderstadt;
+
+import com.example.paurevic.pulsderstadt.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Paurevic on 27.06.2015.
+ */
+public class Person {
+
+        String name;
+        String age;
+        int photoId;
+
+      public Person(String name, String age, int photoId) {
+            this.name = name;
+            this.age = age;
+            this.photoId = photoId;
+        }
+    }
+
+
+
